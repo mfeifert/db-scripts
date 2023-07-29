aef431b51e578fbe6aef07251d4983c2ae76533e
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2023-07-28 17:31:59 -0700

    Change lots of double quotes to single quotes

--- shows.py
+++ shows.py
@@ -8,7 +8,7 @@ else:
     path = '/Users/maf/docs/db/'
 
 # change the string part of value of this variable to "test.db" for testing
-media_db = path + "media.db"
+media_db = path + 'media.db'
 
 # Key = table name, Value = show title
 shows = {
@@ -18,16 +18,19 @@ shows = {
     'cosmos': 'Cosmos',
     'connections': 'Connections',
     'growingup': 'Growing Up in the Universe',
-    'mk': 'Master Keaton',
     'xfiles': 'The X-Files',
-    'twilightzone': 'The Twilight Zone'
+    'twilightzone': 'The Twilight Zone',
+    'mk': 'Master Keaton',
+    'lotgh': 'Legend of the Galactic Heroes'
 }
 
 def main():
     index = 1
+    print('')
     for i in list(shows.items()):
        print(f'{index}: {i[1]}')
        index += 1
+    print('')
     selection = int(input()) - 1
     show(list(shows.items())[selection][0])
 
@@ -46,39 +49,39 @@ def sql_select(db, statement):
 def show(table):
     db = media_db
     menu = [
-    "",
-    "1: List episodes",
-    "2: Next episode",
-    "3: Random episode",
-    "4: Mark episode as watched"
+    '',
+    '1: List episodes',
+    '2: Next episode',
+    '3: Random episode',
+    '4: Mark episode as watched'
     ]
-    if table == "xfiles":
-        menu.append("m: List mythology arc")
+    if table == 'xfiles':
+        menu.append('m: List mythology arc')
     for i in menu:
         print(i)
-    choice = input("\n")
+    choice = input('\n')
     select_all = f"SELECT * from {table}"
     select_next = f"SELECT * FROM {table} WHERE WatchedDate IS NULL ORDER BY NumOverall LIMIT 1"
     select_random = f"SELECT * FROM {table} WHERE WatchedDate IS NULL ORDER BY random() LIMIT 1"
     select_mytharc = f"SELECT * FROM {table} WHERE Mytharc IS NOT NULL"
-    if choice == "1":
-        print("")
+    if choice == '1':
+        print('')
         sql_select(db, select_all)
-        print("")
-    elif choice == "2":
-        print("")
+        print('')
+    elif choice == '2':
+        print('')
         sql_select(db, select_next)
-        print("")
-    elif choice == "3":
-        print("")
+        print('')
+    elif choice == '3':
+        print('')
         sql_select(db, select_random)
-        print("")
-    elif choice == "4":
-        episode = int(input("\nEpisode: "))
+        print('')
+    elif choice == '4':
+        episode = int(input('\nEpisode: '))
         update_episode = f"UPDATE {table} SET WatchedDate = date('now', 'localtime') WHERE NumOverall = {episode}"
         sql_statement(db, update_episode)
-        print("")
-    elif choice == "m":
+        print('')
+    elif choice == 'm':
         sql_select(db, select_mytharc)
 
 main()
