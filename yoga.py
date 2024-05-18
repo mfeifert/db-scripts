e012e8f7a74b80c58ee8cea989fa0c0f7b162b2c
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2024-05-18 15:59:27 -0700

    yoga: Add the ability to enter minutes as an argument

--- yoga.py
+++ yoga.py
@@ -5,23 +5,26 @@ import sqlite3
 import subprocess
 
 if len(sys.argv) < 2:
-    print("Usage: yoga FILE")
-    sys.exit(1)
+    print("Usage: yoga FILE [minutes]")
+    exit()
 
 db_path = sys.argv[1]
 table = 'yoga'
 
 
 def main():
-    try:
-        minutes = input('Minutes: ')
-        sql_log_entry = f"INSERT INTO {table} VALUES (date('now', 'localtime'), {minutes})"
-        sql_log_report = f"SELECT * FROM {table} ORDER BY Date DESC LIMIT 5"
-        issue_sql_statement(db_path, sql_log_entry)
-        print_report(db_path, sql_log_report)
-    except (KeyboardInterrupt, EOFError):
-        print()
-        exit()
+    if len(sys.argv) == 3:
+        minutes = sys.argv[2]
+    else:
+        try:
+            minutes = input('Minutes: ')
+        except (KeyboardInterrupt, EOFError):
+            print()
+            exit()
+    sql_log_entry = f"INSERT INTO {table} VALUES (date('now', 'localtime'), {minutes})"
+    sql_log_report = f"SELECT * FROM {table} ORDER BY Date DESC LIMIT 5"
+    issue_sql_statement(db_path, sql_log_entry)
+    print_report(db_path, sql_log_report)
 
 
 def issue_sql_statement(db, statement):
