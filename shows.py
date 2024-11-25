1fc434ea703ae90fc65e2222db16c5dbcc1ca8da
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2024-11-24 22:17:46 -0800

    shows: Fix indentation and line length

--- shows.py
+++ shows.py
@@ -12,24 +12,24 @@ db_path = sys.argv[1]
 
 # 'table name': 'show title'
 shows = {
-    'tos': 'Star Trek: The Original Series',
-    'tas': 'Star Trek: The Animated Series',
-    'tng': 'Star Trek: The Next Generation',
-    'ds9': 'Star Trek: Deep Space Nine',
-    'stv': 'Star Trek: Voyager',
-    'ste': 'Star Trek: Enterprise',
-    'babylon5': 'Babylon 5',
-    'cosmos': 'Cosmos',
-    'connections': 'Connections',
-    'growingup': 'Growing Up in the Universe',
-    'xfiles': 'The X-Files',
-    'twilightzone': 'The Twilight Zone',
-    'robot':'Mr. Robot',
-    'ft': 'Fawlty Towers',
-    'mk': 'Master Keaton',
-    'monster': 'Monster',
-    'lotgh': 'Legend of the Galactic Heroes',
-    'rolw': 'Record of Lodoss War'
+    "tos": "Star Trek: The Original Series",
+    "tas": "Star Trek: The Animated Series",
+    "tng": "Star Trek: The Next Generation",
+    "ds9": "Star Trek: Deep Space Nine",
+    "stv": "Star Trek: Voyager",
+    "ste": "Star Trek: Enterprise",
+    "babylon5": "Babylon 5",
+    "cosmos": "Cosmos",
+    "connections": "Connections",
+    "growingup": "Growing Up in the Universe",
+    "xfiles": "The X-Files",
+    "twilightzone": "The Twilight Zone",
+    "robot": "Mr. Robot",
+    "ft": "Fawlty Towers",
+    "mk": "Master Keaton",
+    "monster": "Monster",
+    "lotgh": "Legend of the Galactic Heroes",
+    "rolw": "Record of Lodoss War",
 }
 
 
@@ -37,8 +37,8 @@ def main():
     index = 1
     print()
     for i in list(shows.items()):
-       print(f'{index}. {i[1]}')
-       index += 1
+        print(f"{index}. {i[1]}")
+        index += 1
     print()
     try:
         selection = int(input("> ")) - 1
@@ -62,42 +62,46 @@ def print_report(db, statement):
 
 def print_menu_for_selected_show(table):
     menu = [
-    '',
-    '1. List episodes',
-    '2. Next episode',
-    '3. Random episode',
-    '4. Mark episode as watched',
-    'm. Main menu',
+        "",
+        "1. List episodes",
+        "2. Next episode",
+        "3. Random episode",
+        "4. Mark episode as watched",
+        "m. Main menu",
     ]
-    if table == 'xfiles':
-        menu.append('a. List mythology arc')
+    if table == "xfiles":
+        menu.append("a. List mythology arc")
     for i in menu:
         print(i)
     sql_all_episodes = f"SELECT * from {table}"
-    sql_next_episode = f"SELECT * FROM {table} WHERE WatchedDate IS NULL ORDER BY NumOverall LIMIT 1"
-    sql_random_episode = f"SELECT * FROM {table} WHERE WatchedDate IS NULL ORDER BY random() LIMIT 1"
+    sql_next_episode = f"SELECT * FROM {table} \
+        WHERE WatchedDate IS NULL ORDER BY NumOverall LIMIT 1"
+    sql_random_episode = f"SELECT * FROM {table} \
+        WHERE WatchedDate IS NULL ORDER BY random() LIMIT 1"
     sql_mytharc_episodes = f"SELECT * FROM {table} WHERE Mytharc IS NOT NULL"
 
     try:
         while True:
-            choice = input(f'\n{shows[table]}> ')
-            if choice == '1':
+            choice = input(f"\n{shows[table]}> ")
+            if choice == "1":
                 print()
                 print_report(db_path, sql_all_episodes)
-            elif choice == '2':
+            elif choice == "2":
                 print()
                 print_report(db_path, sql_next_episode)
-            elif choice == '3':
+            elif choice == "3":
                 print()
                 print_report(db_path, sql_random_episode)
-            elif choice == '4':
-                episode = int(input('\nEpisode: '))
-                sql_update_episode = f"UPDATE {table} SET WatchedDate = date('now', 'localtime') WHERE NumOverall = {episode}"
+            elif choice == "4":
+                episode = int(input("\nEpisode: "))
+                sql_update_episode = f"UPDATE {table} \
+                    SET WatchedDate = date('now', 'localtime') \
+                    WHERE NumOverall = {episode}"
                 issue_sql_statement(db_path, sql_update_episode)
-            elif choice == 'm':
+            elif choice == "m":
                 print()
                 main()
-            elif choice == 'a':
+            elif choice == "a":
                 print_report(db_path, sql_mytharc_episodes)
     except (KeyboardInterrupt, EOFError):
         print()
